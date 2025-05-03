f02aba6a490d64d01e74abb3bf54344911a02404
Nahom <8nahom8@gmail.com>
2025-05-02 21:41:28 -0500

    added getData func

--- StocksPrediction.py
+++ StocksPrediction.py
@@ -3,3 +3,14 @@ import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
+dates = []
+prices = []
+
+def get_data(filename):
+    with open(filename, "r") as csvFile:
+        csvFileReader = csv.reader(csvFile)
+        next(csvFileReader)
+        for row in csvFileReader:
+            dates.append(int(row([0].split("-")[0])))
+            prices.append(float(row[1]))
+    return
